f178182ccc7db91db52f80ffebe0ea3d4bb23a0f
smter <smtersoyoglu231@gmail.com>
2025-03-19 20:54:40 +0300

    feat: Implement network module with Retrofit and OkHttp

---
+++ app/src/main/java/com/smtersoyoglu/movieapp/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.smtersoyoglu.movieapp.di
+
+import com.smtersoyoglu.movieapp.common.Constants.BASE_URL
+import com.smtersoyoglu.movieapp.common.Constants.TMDB_BEARER_TOKEN
+import com.smtersoyoglu.movieapp.data.source.remote.MovieService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Accept", "application/json")
+                    .addHeader("Authorization", "Bearer $TMDB_BEARER_TOKEN")
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieService(retrofit: Retrofit): MovieService {
+        return retrofit.create(MovieService::class.java)
+    }
+}
