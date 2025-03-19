db595bf60156520c16de8e772af4d6aec7a7ae3c
smter <smtersoyoglu231@gmail.com>
2025-03-19 20:55:48 +0300

    feat: Add base URL and TMDB bearer token constant

--- app/src/main/java/com/smtersoyoglu/movieapp/common/Constants.kt
+++ app/src/main/java/com/smtersoyoglu/movieapp/common/Constants.kt
@@ -1,4 +1,9 @@
 package com.smtersoyoglu.movieapp.common
 
 object Constants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val TMDB_BEARER_TOKEN= "TMDB_BEARER_TOKEN"
+
+
 }
