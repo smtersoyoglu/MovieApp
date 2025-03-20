cf3f469afea6d82f93f5dea0b5a6a4abc61765f6
smter <smtersoyoglu231@gmail.com>
2025-03-20 17:57:43 +0300

    feat: Implement use cases for movie categories

---
+++ app/src/main/java/com/smtersoyoglu/movieapp/domain/usecase/GetPopularMoviesUseCase.kt
@@ -0,0 +1,20 @@
+package com.smtersoyoglu.movieapp.domain.usecase
+
+import com.smtersoyoglu.movieapp.common.Resource
+import com.smtersoyoglu.movieapp.domain.model.Movie
+import com.smtersoyoglu.movieapp.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetPopularMoviesUseCase @Inject constructor(
+    private val repository: MovieRepository,
+) {
+    operator fun invoke(
+        language: String = "en-US",
+        page: Int = 1,
+        region: String? = null,
+    ): Flow<Resource<List<Movie>>> {
+        return repository.getPopularMovies(language, page, region)
+    }
+
+}
---
+++ app/src/main/java/com/smtersoyoglu/movieapp/domain/usecase/GetTopRatedMoviesUseCase.kt
@@ -0,0 +1,19 @@
+package com.smtersoyoglu.movieapp.domain.usecase
+
+import com.smtersoyoglu.movieapp.common.Resource
+import com.smtersoyoglu.movieapp.domain.model.Movie
+import com.smtersoyoglu.movieapp.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetTopRatedMoviesUseCase @Inject constructor(
+    private val movieRepository: MovieRepository,
+) {
+    operator fun invoke(
+        language: String = "en-US",
+        page: Int = 1,
+        region: String? = null,
+    ) : Flow<Resource<List<Movie>>> {
+        return movieRepository.getTopRatedMovies(language, page, region)
+    }
+}
