43b94b876ec218b33ada8b4d5785da26ce482f44
smter <smtersoyoglu231@gmail.com>
2025-03-20 17:58:38 +0300

    feat: Implement HomeViewModel and UiState for movie data fetching

---
+++ app/src/main/java/com/smtersoyoglu/movieapp/presentation/home/HomeViewModel.kt
@@ -0,0 +1,92 @@
+package com.smtersoyoglu.movieapp.presentation.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.smtersoyoglu.movieapp.common.Resource
+import com.smtersoyoglu.movieapp.domain.usecase.GetNowPlayingMoviesUseCase
+import com.smtersoyoglu.movieapp.domain.usecase.GetPopularMoviesUseCase
+import com.smtersoyoglu.movieapp.domain.usecase.GetTopRatedMoviesUseCase
+import com.smtersoyoglu.movieapp.domain.usecase.GetUpcomingMoviesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getNowPlayingMoviesUseCase: GetNowPlayingMoviesUseCase,
+    private val getPopularMoviesUseCase: GetPopularMoviesUseCase,
+    private val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase,
+    private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    init {
+        getNowPlayingMovies()
+        getPopularMovies()
+        getTopRatedMovies()
+        getUpcomingMovies()
+    }
+
+    private fun getNowPlayingMovies() {
+        getNowPlayingMoviesUseCase()
+            .onStart { updateUiState { copy(isLoading = true) } }
+            .onCompletion { updateUiState { copy(isLoading = false) } }
+            .onEach { resource ->
+                when (resource) {
+                    is Resource.Success -> { updateUiState { copy(nowPlayingMovieList = resource.data ?: emptyList()) } }
+                    is Resource.Error -> { updateUiState { copy(error = resource.message ?: "Unknown error") } }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    private fun getPopularMovies() {
+        getPopularMoviesUseCase()
+            .onStart { updateUiState { copy(isLoading = true) } }
+            .onCompletion { updateUiState { copy(isLoading = false) } }
+            .onEach { resource ->
+                when (resource) {
+                    is Resource.Success -> { updateUiState { copy(popularMovieList = resource.data ?: emptyList()) } }
+                    is Resource.Error -> { updateUiState { copy(error = resource.message ?: "Unknown error") } }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    private fun getTopRatedMovies() {
+        getTopRatedMoviesUseCase()
+            .onStart { updateUiState { copy(isLoading = true) } }
+            .onCompletion { updateUiState { copy(isLoading = false) } }
+            .onEach { resource ->
+                when (resource) {
+                    is Resource.Success -> { updateUiState { copy(topRatedMovieList = resource.data ?: emptyList()) } }
+                    is Resource.Error -> { updateUiState { copy(error = resource.message ?: "Unknown error") } }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    private fun getUpcomingMovies() {
+        getUpcomingMoviesUseCase()
+            .onStart { updateUiState { copy(isLoading = true) } }
+            .onCompletion { updateUiState { copy(isLoading = false) } }
+            .onEach { resource ->
+                when (resource) {
+                    is Resource.Success -> { updateUiState { copy(upcomingMovieList = resource.data ?: emptyList()) } }
+
+                    is Resource.Error -> { updateUiState { copy(error = resource.message ?: "Unknown error") } }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+
+    private fun updateUiState(block: UiState.() -> UiState) {
+        _uiState.update(block)
+    }
+}
---
+++ app/src/main/java/com/smtersoyoglu/movieapp/presentation/home/UiState.kt
@@ -0,0 +1,13 @@
+package com.smtersoyoglu.movieapp.presentation.home
+
+import com.smtersoyoglu.movieapp.domain.model.Movie
+
+data class UiState(
+    val isLoading: Boolean = false,
+    val list: List<String> = emptyList(),
+    val error: String? = null,
+    val nowPlayingMovieList: List<Movie> = emptyList(),
+    val popularMovieList: List<Movie> = emptyList(),
+    val topRatedMovieList: List<Movie> = emptyList(),
+    val upcomingMovieList: List<Movie> = emptyList(),
+)
