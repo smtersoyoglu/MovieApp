07657d9c502091b01c2bdee95b211f277c6dc522
smter <smtersoyoglu231@gmail.com>
2025-03-21 21:16:48 +0300

    feat: Add icons for search, home, and favorite

---
+++ app/src/main/java/com/smtersoyoglu/movieapp/presentation/favorite/FavoriteScreen.kt
@@ -0,0 +1,8 @@
+package com.smtersoyoglu.movieapp.presentation.favorite
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavoriteScreen() {
+
+}
---
+++ app/src/main/java/com/smtersoyoglu/movieapp/presentation/search/SearchScreen.kt
@@ -0,0 +1,8 @@
+package com.smtersoyoglu.movieapp.presentation.search
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchScreen() {
+
+}
