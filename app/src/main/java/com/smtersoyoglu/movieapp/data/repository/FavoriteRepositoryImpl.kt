67449018512e5d1be262726461bbb69926a93aaa
smter <smtersoyoglu231@gmail.com>
2025-04-09 18:18:43 +0300

    feat: Implement FavoriteRepository and FavoriteRepositoryImpl

---
+++ app/src/main/java/com/smtersoyoglu/movieapp/data/repository/FavoriteRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.smtersoyoglu.movieapp.data.repository
+
+import com.smtersoyoglu.movieapp.data.mapper.toFavoriteMovie
+import com.smtersoyoglu.movieapp.data.mapper.toFavoriteMovieEntity
+import com.smtersoyoglu.movieapp.data.source.local.FavoriteDao
+import com.smtersoyoglu.movieapp.domain.model.FavoriteMovie
+import com.smtersoyoglu.movieapp.domain.repository.FavoriteRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class FavoriteRepositoryImpl @Inject constructor(
+    private val favoriteDao: FavoriteDao
+) : FavoriteRepository {
+
+    override suspend fun insertFavorite(favoriteMovie: FavoriteMovie) = favoriteDao.insertFavorite(favoriteMovie.toFavoriteMovieEntity())
+
+    override suspend fun deleteFavorite(favoriteMovie: FavoriteMovie) = favoriteDao.deleteFavorite(favoriteMovie.toFavoriteMovieEntity())
+
+    override fun getAllFavorites(): Flow<List<FavoriteMovie>> = favoriteDao.getAllFavorites().map { it.map { it.toFavoriteMovie() } }
+
+    override suspend fun isFavorite(movieId: Int): Boolean = favoriteDao.isFavorite(movieId)
+}
---
+++ app/src/main/java/com/smtersoyoglu/movieapp/domain/repository/FavoriteRepository.kt
@@ -0,0 +1,15 @@
+package com.smtersoyoglu.movieapp.domain.repository
+
+import com.smtersoyoglu.movieapp.domain.model.FavoriteMovie
+import kotlinx.coroutines.flow.Flow
+
+interface FavoriteRepository {
+
+    suspend fun insertFavorite(favoriteMovie: FavoriteMovie)
+
+    suspend fun deleteFavorite(favoriteMovie: FavoriteMovie)
+
+    fun getAllFavorites(): Flow<List<FavoriteMovie>>
+
+    suspend fun isFavorite(movieId: Int): Boolean
+}
