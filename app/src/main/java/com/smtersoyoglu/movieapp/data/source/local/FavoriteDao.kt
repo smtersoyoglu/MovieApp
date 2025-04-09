790c8455242251070620a723571bb6f0a03376e0
smter <smtersoyoglu231@gmail.com>
2025-04-09 18:06:26 +0300

    feat: Implement FavoriteMovieEntity and update favorite movie database operations

--- app/src/main/java/com/smtersoyoglu/movieapp/data/source/local/FavoriteDao.kt
+++ app/src/main/java/com/smtersoyoglu/movieapp/data/source/local/FavoriteDao.kt
@@ -5,22 +5,21 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.smtersoyoglu.movieapp.data.source.local.entity.MovieEntity
+import com.smtersoyoglu.movieapp.data.source.local.entity.FavoriteMovieEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertFavorite(movieEntity: MovieEntity)
+    suspend fun insertFavorite(favoriteMovieEntity: FavoriteMovieEntity)
 
     @Delete
-    suspend fun deleteFavorite(movieEntity: MovieEntity)
+    suspend fun deleteFavorite(favoriteMovieEntity: FavoriteMovieEntity)
 
     @Query("SELECT * FROM favorites")
-    fun getAllFavorites(): Flow<List<MovieEntity>>
+    fun getAllFavorites(): Flow<List<FavoriteMovieEntity>>
 
-    @Query("SELECT * FROM favorites WHERE id = :movieId")
-    suspend fun getFavoriteById(movieId: Int): MovieEntity?
-
+    @Query("SELECT EXISTS(SELECT 1 FROM favorites WHERE id = :movieId)")
+    suspend fun isFavorite(movieId: Int): Boolean
 }
--- app/src/main/java/com/smtersoyoglu/movieapp/data/source/local/entity/FavoriteMovieEntity.kt
+++ app/src/main/java/com/smtersoyoglu/movieapp/data/source/local/entity/FavoriteMovieEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "favorites")
-data class MovieEntity(
+data class FavoriteMovieEntity(
     @PrimaryKey val id: Int,
     val title: String,
     val posterPath: String?
