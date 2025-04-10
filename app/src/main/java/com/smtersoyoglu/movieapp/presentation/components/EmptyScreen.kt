64f00f6b1913ef21cef45ba68c7a45e7467b526e
smter <smtersoyoglu231@gmail.com>
2025-04-10 19:43:41 +0300

    feat: Add EmptySearchState composable

--- app/src/main/java/com/smtersoyoglu/movieapp/presentation/components/EmptyScreen.kt
+++ app/src/main/java/com/smtersoyoglu/movieapp/presentation/components/EmptyScreen.kt
@@ -1,14 +1,24 @@
 package com.smtersoyoglu.movieapp.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.smtersoyoglu.movieapp.R
 
 @Composable
 fun EmptyScreen(message: String, modifier: Modifier = Modifier) {
@@ -21,6 +31,36 @@ fun EmptyScreen(message: String, modifier: Modifier = Modifier) {
             color = Color.Gray,
             fontSize = 16.sp,
             textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun EmptySearchState(query: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_empty_search),
+            contentDescription = "Empty Search",
+            tint = Color.White,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "No Movies Found",
+            style = MaterialTheme.typography.titleLarge,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "We couldn't find any movies matching \"$query\".\nTry different keywords or check your spelling.",
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center,
+            color = Color.White
         )
     }
 }
