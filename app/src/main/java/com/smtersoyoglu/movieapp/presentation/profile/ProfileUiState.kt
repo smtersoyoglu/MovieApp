24b2a2d77cb6581973689a2f07bf896f4559d970
smter <smtersoyoglu231@gmail.com>
2025-04-23 23:08:27 +0300

    feat: Implement ProfileViewModel and ProfileUiState

---
+++ app/src/main/java/com/smtersoyoglu/movieapp/presentation/profile/ProfileUiState.kt
@@ -0,0 +1,11 @@
+package com.smtersoyoglu.movieapp.presentation.profile
+
+import com.smtersoyoglu.movieapp.domain.model.UserProfile
+
+data class ProfileUiState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val userProfile: UserProfile? = null,
+    val showSignOutDialog: Boolean = false,
+    val isSignedOut: Boolean = false
+)
---
+++ app/src/main/java/com/smtersoyoglu/movieapp/presentation/profile/ProfileViewModel.kt
@@ -0,0 +1,62 @@
+package com.smtersoyoglu.movieapp.presentation.profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.smtersoyoglu.movieapp.common.Resource
+import com.smtersoyoglu.movieapp.domain.usecase.GetUserProfileUseCase
+import com.smtersoyoglu.movieapp.domain.usecase.SignOutUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val getUserProfileUseCase: GetUserProfileUseCase,
+    private val signOutUseCase: SignOutUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ProfileUiState())
+    val uiState: StateFlow<ProfileUiState> = _uiState.asStateFlow()
+
+    init {
+        fetchUserProfile()
+    }
+
+    private fun fetchUserProfile() {
+        viewModelScope.launch {
+            updateUiState { copy(isLoading = true, error = null) }
+            when (val result = getUserProfileUseCase()) {
+                is Resource.Success -> updateUiState { copy(isLoading = false, userProfile = result.data) }
+                is Resource.Error -> updateUiState { copy(isLoading = false, error = result.message) }
+            }
+        }
+    }
+
+    fun onSignOutClicked() {
+        updateUiState { copy(showSignOutDialog = true) }
+    }
+
+    fun onSignOutConfirmed() {
+        viewModelScope.launch {
+            updateUiState{ copy(isLoading = true, showSignOutDialog = false) }
+            when (val result = signOutUseCase()) {
+                is Resource.Success -> updateUiState { copy(isLoading = false, isSignedOut = true) }
+                is Resource.Error -> updateUiState { copy(isLoading = false, error = result.message) }
+            }
+        }
+    }
+
+    fun onSignOutDialogDismissed() {
+        updateUiState { copy(showSignOutDialog = false) }
+    }
+
+
+    private fun updateUiState(block: ProfileUiState.() -> ProfileUiState) {
+        _uiState.update(block)
+    }
+
+}
